a91c81803b283b4807d0962bccf44de75aeba043
betterthankobe <wangzheng0521@yeah.net>
2026-03-21 14:27:40 +0800

    Initial Commit

---
+++ BetterTrain/BetterTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterTrainApp.swift
+//  BetterTrain
+//
+//  Created by 王峥 on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
